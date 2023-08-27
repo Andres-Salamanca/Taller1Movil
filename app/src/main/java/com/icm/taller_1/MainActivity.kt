84a404c9f338dfcb20f13da12027b73036185c5b
ALEYI17 <aleelaleyi@gmail.com>
2023-08-27 15:38:06 -0500

    Ya casi se yermina el tic tac toe

--- app/src/main/java/com/icm/taller_1/MainActivity.kt
+++ app/src/main/java/com/icm/taller_1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.icm.taller_1
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -37,6 +38,10 @@ class MainActivity : AppCompatActivity() {
 //            Toast.makeText(applicationContext, "Hola", Toast.LENGTH_LONG).show()
 //        }
 
+        juego.setOnClickListener {
+            val intent = Intent(this, TicTacToe::class.java)
+            startActivity(intent)
+        }
 
     }
 
