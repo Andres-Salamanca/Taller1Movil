3091ebff6b28ada831dedd9e6d5e84762774bf89
ALEYI17 <aleelaleyi@gmail.com>
2023-08-27 12:10:58 -0500

    Puse los anclajes de los botones

--- app/src/main/java/com/icm/taller_1/MainActivity.kt
+++ app/src/main/java/com/icm/taller_1/MainActivity.kt
@@ -22,20 +22,20 @@ class MainActivity : AppCompatActivity() {
 
         var pais: Button = findViewById<Button>(R.id.pais) //Boton de pais
 
-        var spinner: Spinner = findViewById(R.id.spinner) //Spinner de pises
+        var spinner: Spinner = findViewById(R.id.spinner) //Spinner de paises
         var adaptador = ArrayAdapter<String>(this, R.array.idiomas, android.R.layout.simple_list_item_1)
         spinner.adapter = adaptador
 
-        when(spinner.selectedItem.toString()) { //Obtener el elemento que se esta seleccionando dentro del spinner y se convierte en string para asignar un valor
-            "Español" -> saludo.text = "Bienvenido a TicTacToe" //Se le asigna un valor a la opción escogida
-            "English - Ingles" -> saludo.text = "Welcome to TicTacToe" //Se le asigna un valor a la opción escogida
-            "Français - Frances" -> saludo.text = "Bienvenue à TicTacToe" //Se le asigna un valor a la opción escogida
-            "Deutsch - Aleman" -> saludo.text = "Willkommen bei TicTacToe" //Se le asigna un valor a la opción escogida
-            "日本語 - Japones" -> saludo.text = "TicTacToe へようこそ" //Se le asigna un valor a la opción escogida
-        }
-        saludo.setOnClickListener {
-            Toast.makeText(applicationContext, "Hola", Toast.LENGTH_LONG).show()
-        }
+//        when(spinner.selectedItem.toString()) { //Obtener el elemento que se esta seleccionando dentro del spinner y se convierte en string para asignar un valor
+//            "Español" -> saludo.text = "Bienvenido a TicTacToe" //Se le asigna un valor a la opción escogida
+//            "English - Ingles" -> saludo.text = "Welcome to TicTacToe" //Se le asigna un valor a la opción escogida
+//            "Français - Frances" -> saludo.text = "Bienvenue à TicTacToe" //Se le asigna un valor a la opción escogida
+//            "Deutsch - Aleman" -> saludo.text = "Willkommen bei TicTacToe" //Se le asigna un valor a la opción escogida
+//            "日本語 - Japones" -> saludo.text = "TicTacToe へようこそ" //Se le asigna un valor a la opción escogida
+//        }
+//        saludo.setOnClickListener {
+//            Toast.makeText(applicationContext, "Hola", Toast.LENGTH_LONG).show()
+//        }
 
 
     }
