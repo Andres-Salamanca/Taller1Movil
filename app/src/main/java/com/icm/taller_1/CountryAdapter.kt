22b90c269d2673a814128e249edfa3f4f24910aa
Andres <salamanca-andres@javeriana.edu.co>
2023-08-27 22:22:02 -0500

    Ya estan las fotos de las banderas

--- app/src/main/java/com/icm/taller_1/CountryAdapter.kt
+++ app/src/main/java/com/icm/taller_1/CountryAdapter.kt
@@ -4,8 +4,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
-
+import com.squareup.picasso.Picasso
 
 class CountryAdapter(private val context: Context, private val countries: List<PaisesActivity.Country>) : BaseAdapter() {
     override fun getCount(): Int = countries.size
@@ -21,11 +22,13 @@ class CountryAdapter(private val context: Context, private val countries: List<P
 
 
         // Find views using findViewById
+        val imageview = view.findViewById<ImageView>(R.id.imageView2)
         val textView2 = view.findViewById<TextView>(R.id.textView3)
         val textView3 = view.findViewById<TextView>(R.id.textView5)
         val textView4 = view.findViewById<TextView>(R.id.textView7)
         val textView5 = view.findViewById<TextView>(R.id.textView9)
 
+        Picasso.get().load("https://flagsapi.com/${country.sigla}/flat/64.png").into(imageview)
         // Populate the views with data
         textView2.text = country.nombre_pais
         textView3.text =  country.nombre_pais_int
